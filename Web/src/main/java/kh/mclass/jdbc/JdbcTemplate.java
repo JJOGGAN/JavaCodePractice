4a049bc924008e6c891f1feedd95b8f3aa92ab92
JJOGGAN <withmelona1@gmail.com>
2024-04-03 20:31:43 +0900

    getSemiConnection 생성
    getSemiConnection 생성하여 게시판 작성 db에 연결

--- Web/src/main/java/kh/mclass/jdbc/JdbcTemplate.java
+++ Web/src/main/java/kh/mclass/jdbc/JdbcTemplate.java
@@ -10,7 +10,32 @@ import java.util.Properties;
 public class JdbcTemplate {
 	private JdbcTemplate() {}
 	
+	//SEMIM을 위한 connection
+	public static Connection getSemiConnection(boolean islocalhost) {
+		Connection conn = null;
+		Properties prop = new Properties(); // 애플리케이션의 환경설정과 관련된 속성을 저장 key:value
+		try {
+			String currentPath = JdbcTemplate.class.getResource("").getPath();
+			prop.load(new FileReader(currentPath + "driver.properties")); /* properties 파일 연결 */
+			Class.forName(prop.getProperty("jdbc.driver"));
+			if(islocalhost) {
+				conn = DriverManager.getConnection(prop.getProperty("jdbc.url")
+						, prop.getProperty("jdbc.semi.username")
+						, prop.getProperty("jdbc.semi.password"));
+			}else {
+				conn = DriverManager.getConnection(prop.getProperty("jdbc.semim.dbserver.url")
+						, prop.getProperty("jdbc.semi.username")
+						, prop.getProperty("jdbc.semi.password"));
+			}
+
+			if(conn != null) System.out.println("연결성공"); else System.out.println("연결실패!!!!!!!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 	
+	//기존 connection function
 	public static Connection getConnection() {
 		Connection conn = null;
 		Properties prop = new Properties(); // 애플리케이션의 환경설정과 관련된 속성을 저장 key:value
@@ -18,6 +43,7 @@ public class JdbcTemplate {
 			String currentPath = JdbcTemplate.class.getResource("").getPath();
 			prop.load(new FileReader(currentPath + "driver.properties")); /* properties 파일 연결 */
 			Class.forName(prop.getProperty("jdbc.driver"));
+
 			conn = DriverManager.getConnection(prop.getProperty("jdbc.url")
 											, prop.getProperty("jdbc.username")
 											, prop.getProperty("jdbc.password"));
