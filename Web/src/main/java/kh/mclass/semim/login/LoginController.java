bd1fb8aaac121c15580e73b8e83bcb5e77088acc
JJOGGAN <withmelona1@gmail.com>
2024-04-08 12:17:09 +0900

    수정
    resultInfo의 정보의 유무로 if문을 작성했기 때문에 0이 아니라 null을 기준으로 if문이 작성되어야 한다

--- Web/src/main/java/kh/mclass/semim/login/LoginController.java
+++ Web/src/main/java/kh/mclass/semim/login/LoginController.java
@@ -42,16 +42,15 @@ public class LoginController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
-	
 		MemberLoginDto dto = new MemberLoginDto(id, pwd);
-		System.out.println("/login doPost dto : "+dto);
+		System.out.println("/login doPost dto: "+ dto);
 		//TODO login - 실습 때 구현 - classdiagram참고
 		//성공 : 1
 		//실패 : 0
 		int result = 0;
 		//int result = new MemberService().login(dto); 
-		MemberInfoDto resultInfo = new MemberService().loginGetInfo(dto);//로그인 했을 때 각종 정보를 가져오는 것
-		if (result > 0) {//succcess
+		MemberInfoDto resultInfo = new MemberService().loginGetInfo(dto);;//로그인 했을 때 각종 정보를 가져오는 것
+		if (resultInfo != null) {//succcess
 		//	request.getSession().setAttribute("ssslogin", id); // setAttribute (이름, 저장할 데이터) , 값이 많다면..pwd를 가지고 다니면 위험하니까 memberInfoDto를 가지고 다녀도 될 듯
 			request.getSession().setAttribute("ssslogin", resultInfo);//이름이 겹칠 가능성이 있다면 특이하게 적어서 겹칠 수 있는 경우를 차단
 			result=1;
