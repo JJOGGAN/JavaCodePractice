e7617d2d8e0a1e68b105db56ccd29e1ad7ccf0ff
JJOGGAN <withmelona1@gmail.com>
2024-04-16 00:16:04 +0900

    게시판 글쓰기, 댓글쓰기 controller

---
+++ Web/src/main/java/kh/mclass/semim/board/controller/BoardReplyWriteController.java
@@ -0,0 +1,83 @@
+package kh.mclass.semim.board.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import kh.mclass.semim.board.model.dto.BoardReplyListDto;
+import kh.mclass.semim.board.model.dto.BoardReplyWriteDto;
+import kh.mclass.semim.board.model.service.BoardService;
+
+/**
+ * Servlet implementation class BoardReplyWriteController
+ */
+@WebServlet("/board/reply/write.ajax")
+public class BoardReplyWriteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BoardService service = new BoardService();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardReplyWriteController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/board/reply/write.ajax doPost()");
+		String boardReplyIdStr = request.getParameter("boardReplyId");
+		int boardReplyId=0;
+		String boardIdStr  = request.getParameter("boardId");
+		int boardId = 0;
+		String boardReplyContent  = request.getParameter("boardReplyContent");
+		//TODO login 수정될 때마다 로그인 하기 번거로우니 하드 코딩으로 고정해 둔 것
+		String boardReplyWriter = "kh1";  //TODO
+		String boardReplyLogIp = null;  // TODO
+		
+		Gson gson = new Gson();
+		
+		// 게시글 작성자의 아이디
+		if(boardIdStr == null || boardIdStr.equals("")) {
+			response.getWriter().append("-1");
+		}
+		
+		//댓글 작성자의 아이디
+		if(boardReplyIdStr != null && !boardReplyIdStr.equals("")) {
+			try {
+				boardReplyId = Integer.parseInt(boardReplyIdStr);
+			}catch (NumberFormatException e) {
+				response.getWriter().append("-1");
+				return;
+			}
+		}
+		if(boardReplyIdStr == null || boardReplyIdStr.equals("")) {
+			// 댓글 원본으로 간주함
+			boardReplyId = 0;
+		}
+		BoardReplyWriteDto dto = new BoardReplyWriteDto(boardReplyId, boardId, boardReplyWriter, boardReplyContent, boardReplyLogIp);
+		System.out.println(dto);
+		int result = service.insertReply(dto);
+//		response.getWriter().append(String.valueOf(result));
+		if(result > 0) {
+			List<BoardReplyListDto> replydtolist = service.selectBoardReplyList(boardId);
+			response.getWriter().append(gson.toJson(replydtolist));
+		}
+	}
+
+}
--- Web/src/main/java/kh/mclass/semim/board/controller/BoardWriteController.java
+++ Web/src/main/java/kh/mclass/semim/board/controller/BoardWriteController.java
@@ -1,5 +1,6 @@
 package kh.mclass.semim.board.controller;
 
+import java.io.File;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.connector.Response;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.oreilly.servlet.multipart.MultipartParser;
 
 import kh.mclass.semim.board.model.dto.BoardInsertDto;
 import kh.mclass.semim.board.model.service.BoardService;
@@ -33,6 +38,31 @@ public class BoardWriteController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String prePage =(String)request.getSession().getAttribute("prePage");
+		String uploadPath  = request.getServletContext().getRealPath("resources/uploadfiles"); //? resource 아래의 upladfiles에 파일 저장
+		//file-즉 enctype="multipart/form-data" 형태로 전달된 경우
+		File uploadPathFile = new File(uploadPath);
+		if(!uploadPathFile.exists()) {
+			uploadPathFile.mkdirs();
+		}
+		int uploadFileLimit = 10*1024*1024; //10M 제한
+		
+//		new MultipartRequest(request //jsp->controller 로 전달된 객체
+//				,getServletInfo() //wqs 서버에 저장할 디렉토리
+//				,serialVersionUID //업로드 파일 크기제한
+//				,// 인코딩 방법
+//				, //was 서버에 저장할 디렉토리에 동일이름이 존재할 때 새로운 이름 부여방식
+//				null);
+		MultipartRequest multiReq=new MultipartRequest(request,uploadPath,uploadFileLimit,new DefaultFileRenamePolicy());
+		
+		String filePath=multiReq.getFilesystemName("uploadfiles");
+		//파일 꺼내기 만약 파일이 없다면 어떻게 출력할 것인가
+		if(filePath == null) {
+			System.out.println("첨부파일이 없다.");
+		}else {
+			System.out.println("첨부파일정보는 ==== ");
+			System.out.println(filePath);
+			System.out.println(multiReq.getOriginalFileName(filePath));//....?????
+		}
 		if(prePage !=null && prePage.equals("write")) {
 			request.getSession().removeAttribute("prePage");
 		}
